34b3aa68a1a3068e542b8c78169c0bae15aa53ea
Justin H <jho1@stevens.edu>
2020-11-26 02:54:14 -0500

    Added Mii toggle

--- assets/board.js
+++ assets/board.js
@@ -14,16 +14,19 @@ $(document).ready(function () {
   "ganondorf.svg", "greninja.svg", "ice_climbers.svg", "ike.svg", "incineroar.svg", "inkling.svg", "isabelle.svg",
   "jigglypuff.svg", "joker.svg", "ken.svg", "king_dedede.svg", "king_k_rool.svg", "kirby.svg", "link.svg",
   "little_mac.svg", "lucario.svg", "lucas.svg", "lucina.svg", "luigi.svg", "mario.svg", "marth.svg", "mega_man.svg",
-  "meta_knight.svg", "mewtwo.svg", "mii_fighter.svg", "minmin.svg", "mr_game_and_watch.svg", "ness.svg", "olimar.svg",
+  "meta_knight.svg", "mewtwo.svg", "minmin.svg", "mr_game_and_watch.svg", "ness.svg", "olimar.svg",
   "pac_man.svg", "packun_flower.svg", "palutena.svg", "peach.svg", "pichu.svg", "pikachu.svg", "pit.svg",
   "pokemon_trainer.svg", "richter.svg", "ridley.svg", "rob.svg", "robin.svg", "rosalina_and_luma.svg", "roy.svg",
   "ryu.svg", "samus.svg", "sheik.svg", "shulk.svg", "simon.svg", "snake.svg", "sonic.svg", "steve.svg", "terry.svg",
   "toon_link.svg", "villager.svg", "wario.svg", "wii_fit_trainer.svg", "wolf.svg", "yoshi.svg", "young_link.svg",
   "zelda.svg", "zero_suit_samus.svg"];
   var dir = "assets/svgs/";
+  const mii = "mii_fighter.svg";
 
   // draws the board; not the best solution, but it works
   function drawBoard () {
+    $("#board").empty();
+
     for (var i = 0; i < files.length; i++) {
       if (i % 10 == 0) {
         $("#board").append("<div class=\"column is-1 is-clickable is-offset-1\"><img class=\"image is-48x48\" src=\"" + dir + files[i] + "\" /> </div>");
@@ -58,7 +61,19 @@ $(document).ready(function () {
     }
 
     shuffleArray(files);
-    $("#board").empty();
     drawBoard();
+  });
+
+  // toggles miis
+  $("#mii").click(function () {
+    if ($(this).prop("checked")) {
+      files.push(mii);
+      drawBoard();
+    } else {
+      const i = files.indexOf(mii);
+      if (i > -1)
+        files.splice(i, 1);
+      drawBoard();
+    }
   });
 });
